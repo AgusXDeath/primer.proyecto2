a371e83ce299682eb84ec69e2b626af95499f691
malenasandoval <malenasando@gmail.com>
2024-05-13 10:33:10 -0300

    creación de navegador y configuración de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
+const routes: Routes = [
+  {
+    path:"",component:InicioComponent
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+  }
+  {
+    path:"",loadChildren:()=>import('./modules/producto/producto.module').then(m=>m.ProductoModule)
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
